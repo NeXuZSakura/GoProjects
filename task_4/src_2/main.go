71a01109772621de489ef4996ddea2481078917f
NeXuZSakura <stanislavkasmak6@gmail.com>
2023-11-20 14:31:50 +0300

    v1.2

--- task_4/src_2/main.go
+++ task_4/src_2/main.go
@@ -2,8 +2,6 @@ package main
 
 //go:generate go build -tags add
 
-var fileString string
-
 var Animals = []string{
 	"cat",
 	"dog",
@@ -14,5 +12,4 @@ func main() {
 	for _, anim := range Animals {
 		println(">", anim)
 	}
-	println(fileString)
 }
